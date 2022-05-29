91cc5fe9c9cdad05b036b212ef4d27be7e842dfc
4nn4h <annahemails@gmail.com>
2022-05-29 03:53:22 -0500

    adds start page

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
-var createStartEl = function() {
+var pageContentEl = document.querySelector("#page-content");
+var headerTextEl = document.querySelector("#header-text");
+var subtextEl = document.querySelector("#subtext");
+var wrapperEl = document.querySelector("#wrapper");
 
+var createStarterPage = function () {
+    var headerItemEl = document.createElement("h1");
+    headerItemEl.className = "header-text";
+    headerItemEl.textContent = "Code Quiz";
+
+    headerTextEl.appendChild(headerItemEl);
+
+    var subtextItemEl = document.createElement("p");
+    subtextItemEl.className = "add-text";
+    subtextItemEl.textContent = "You will have 60 seconds to answer five questions. Each incorrect answer will subtract seconds off the remaining time. The top five scores will be saved."
+
+    subtextEl.appendChild(subtextItemEl);
+
+    var buttonItemEl = document.createElement("button");
+    buttonItemEl.className = "select-btn";
+    buttonItemEl.textContent = "Start Quiz";
+
+    wrapperEl.appendChild(buttonItemEl);
 };
+
