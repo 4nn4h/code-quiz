513e3ff301dcfb28dec88d25b1bb6ccea164b9a6
4nn4h <annahemails@gmail.com>
2022-05-29 05:00:14 -0500

    adds ability to start quiz

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
 var pageContentEl = document.querySelector("#page-content");
-var headerTextEl = document.querySelector("#header-text");
-var subtextEl = document.querySelector("#subtext");
 var wrapperEl = document.querySelector("#wrapper");
 
 var createStarterPage = function () {
@@ -8,18 +6,52 @@ var createStarterPage = function () {
     headerItemEl.className = "header-text";
     headerItemEl.textContent = "Code Quiz";
 
-    headerTextEl.appendChild(headerItemEl);
+    wrapperEl.appendChild(headerItemEl);
 
     var subtextItemEl = document.createElement("p");
     subtextItemEl.className = "add-text";
     subtextItemEl.textContent = "You will have 60 seconds to answer five questions. Each incorrect answer will subtract seconds off the remaining time. The top five scores will be saved."
 
-    subtextEl.appendChild(subtextItemEl);
+    wrapperEl.appendChild(subtextItemEl);
 
     var buttonItemEl = document.createElement("button");
-    buttonItemEl.className = "select-btn";
+    buttonItemEl.className = "start-btn";
     buttonItemEl.textContent = "Start Quiz";
 
     wrapperEl.appendChild(buttonItemEl);
 };
 
+var questionPages = function () {
+    var headerItemEl = document.createElement("h1");
+    headerItemEl.className = "header-text";
+    headerItemEl.textContent = "Test";
+
+    wrapperEl.appendChild(headerItemEl);
+
+    for (i = 0; i < 2; i++){
+        var buttonItemEl = document.createElement("button");
+        buttonItemEl.className = "option-button";
+        buttonItemEl.textContent = "Answer";
+        wrapperEl.appendChild(buttonItemEl);
+    }
+};
+
+var revertToDefault = function () {
+    wrapperEl.remove();
+    wrapperEl = document.createElement("div");
+    wrapperEl.className = "wrapper";
+    wrapperEl.id = "wrapper";
+    pageContentEl.appendChild(wrapperEl);
+};
+
+var buttonHandler = function(event) {
+    targetEl = event.target;
+
+    if (targetEl.matches(".start-btn")) {
+        revertToDefault();
+        questionPages();
+    }
+};
+
+createStarterPage();
+pageContentEl.addEventListener("click", buttonHandler);
